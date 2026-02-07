513014a0c3ac2d4cb7c98d1fa92b29e0cdeabed2
yash <yash@yash>
2026-02-07 23:40:21 +0530

    Add(enum): added slottype, vehicletype,slotstatus enums.

---
+++ Enum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingLotSystem
+{
+    public enum VehicleType
+    {
+        CAR,BIKE,TRUCK
+    }
+
+    public enum SlotStatus
+    {
+        Occupied, Available
+    }
+
+    public enum SlotType
+    {
+        CAR, BIKE, TRUCK
+    }
+}
